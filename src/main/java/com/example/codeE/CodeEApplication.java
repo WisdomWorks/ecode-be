e6c376a4304496ca54abbd3351e34b7b2ccfd34d
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-01-21 16:07:30 +0700

    Init CodeE project

---
+++ src/main/java/com/example/codeE/CodeEApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeE/CodeEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
