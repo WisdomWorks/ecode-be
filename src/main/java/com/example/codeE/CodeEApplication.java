c976661bbec6b582c3245ee42ac2eafb21b26d30
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-01-25 20:38:57 +0700

    feat: config swagger

--- src/main/java/com/example/codeE/CodeEApplication.java
+++ src/main/java/com/example/codeE/CodeEApplication.java
@@ -1,10 +1,14 @@
 package com.example.codeE;
 
 import com.example.codeE.model.user.StudentEnrollment;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CodeEApplication {
 
 	public static void main(String[] args) {
