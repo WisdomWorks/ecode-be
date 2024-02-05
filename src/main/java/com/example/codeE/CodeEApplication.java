2d2e817b3dfd2d03126240bdf3af73719c22af07
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-02-05 20:52:04 +0700

    refactor[WW-116]: add spring fox swagger and configuration

--- src/main/java/com/example/codeE/CodeEApplication.java
+++ src/main/java/com/example/codeE/CodeEApplication.java
@@ -1,5 +1,6 @@
 package com.example.codeE;
 
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableSwagger2
+@Configuration
 public class CodeEApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodeEApplication.class, args);
