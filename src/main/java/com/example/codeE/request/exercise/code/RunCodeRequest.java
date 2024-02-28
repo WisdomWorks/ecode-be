ae1b17cb918c9b2860564de15d25e8ee4fad54e2
Noro911 <hoainhan.lhp@gmail.com>
2024-02-28 18:08:32 +0700

    feat: update run code and submit code features

--- src/main/java/com/example/codeE/request/exercise/code/RunCodeRequest.java
+++ src/main/java/com/example/codeE/request/exercise/code/RunCodeRequest.java
@@ -1,9 +1,12 @@
 package com.example.codeE.request.exercise.code;
 
+import com.example.codeE.model.exercise.common.IOTestCase;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +16,5 @@ public class RunCodeRequest {
     private String containerId;
     private String contentFile;
     private String fileName;
+    private List<IOTestCase> inputs;
 }
