007201811c5df8d09693b8b74e6dc8312fb53fc7
Noro911 <hoainhan.lhp@gmail.com>
2024-02-28 10:03:36 +0700

    fix: fix create code exercise controller

--- src/main/java/com/example/codeE/controller/ExerciseController.java
+++ src/main/java/com/example/codeE/controller/ExerciseController.java
@@ -2,16 +2,29 @@ package com.example.codeE.controller;
 
 import com.example.codeE.model.exercise.CodeExercise;
 import com.example.codeE.model.exercise.Exercise;
+import com.example.codeE.model.exercise.common.TestCase;
 import com.example.codeE.request.exercise.DeleteExerciseRequest;
+import com.example.codeE.request.exercise.code.CreateCodeExerciseRequest;
 import com.example.codeE.service.exercise.CodeExerciseService;
 import com.example.codeE.service.exercise.ExerciseService;
+import com.example.codeE.service.exercise.common.TestcaseService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -25,10 +38,23 @@ public class ExerciseController {
     @Autowired
     private ExerciseService exerciseService;
 
+    @Autowired
+    private TestcaseService testcaseService;
+
     @PostMapping
     @RequestMapping(value = "code",method = RequestMethod.POST)
-    public ResponseEntity<?> createCodeExercise(@Valid @RequestBody CodeExercise exercise){
-        CodeExercise codeExercise = this.codeExerciseService.createCodeExercise(exercise);
+    public ResponseEntity<?> createCodeExercise(@Valid @RequestBody CreateCodeExerciseRequest request){
+        List<String> testcaseIds = new ArrayList<>();
+        for(TestCase tc: request.getTestcases()){
+            String id = this.testcaseService.createTestcase(tc).getTestcaseId();
+            testcaseIds.add(id);
+        }
+        CodeExercise codeExercise = this.codeExerciseService.createCodeExercise(
+                new CodeExercise(request.getTopicId(), request.getExerciseName(), request.getKey(),
+                        request.getStartTime(), request.getEndTime(), request.getType(), request.getPublicGroupIds(),
+                        request.getLanguage(), request.getFunctionName(), request.getTemplate(),
+                        request.getDescription(), testcaseIds)
+        );
         this.exerciseService.saveExercise((Exercise) codeExercise);
         return ResponseEntity.status(HttpStatus.CREATED).body(codeExercise);
     }
