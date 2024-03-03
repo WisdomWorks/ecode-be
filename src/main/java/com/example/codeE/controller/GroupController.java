312da0be149be64f3210d349ddc8c57f9fe1efb3
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-03-03 21:49:21 +0700

    Feat[WW-93] Update group API

--- src/main/java/com/example/codeE/controller/GroupController.java
+++ src/main/java/com/example/codeE/controller/GroupController.java
@@ -1,11 +1,14 @@
 package com.example.codeE.controller;
 
+import com.example.codeE.request.group.CreateGroupRequest;
+
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
-import com.example.codeE.model.group.Group;
 import com.example.codeE.service.group.GroupService;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +28,16 @@ public class GroupController {
     @GetMapping
     @RequestMapping(value = "{groupId}",method = RequestMethod.GET)
     public ResponseEntity<?> getGroupById(@PathVariable String groupId) {
-        return new ResponseEntity<>(groupService.getById(groupId), HttpStatus.OK);
+        var result = this.groupService.getById(groupId);
+        if(result != null){
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }       
+        return ResponseEntity.status(HttpStatus.CREATED).body("Can not find group with id: " + groupId);
     }
 
     @PostMapping
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public ResponseEntity<?> createGroup(@RequestBody Group group) {
+    public ResponseEntity<?> createGroup(@RequestBody CreateGroupRequest group) {
         return new ResponseEntity<>(groupService.createOne(group), HttpStatus.CREATED);
     }
 
@@ -38,9 +45,19 @@ public class GroupController {
     // public ResponseEntity<?> updateGroup(String groupId, @RequestBody Group group) {
     //     return new ResponseEntity<>(groupService.updateGroupById(groupId, group), HttpStatus.OK);
     // }
-    // @DeleteMapping("/{groupId}")
-    // public ResponseEntity<?> deleteGroup(String groupId) {
-    //     groupService.deleteById(groupId);
-    //     return new ResponseEntity<>(HttpStatus.OK);
-    // }
+    @DeleteMapping
+    @RequestMapping(value = "{groupId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteGroup(String groupId) {
+        var result = this.groupService.deleteById(groupId);
+        if (result) {
+            return ResponseEntity.ok(Map.of("message" , "Delete group successfully"));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No course found with ID:" + groupId);
+    }
+
+    @GetMapping
+    @RequestMapping(value = "/get-groups-in-course/{courseId}", method = RequestMethod.GET)
+    public  ResponseEntity<?> getGroupsByCourseId(@PathVariable String courseId){
+        return ResponseEntity.status(HttpStatus.OK).body(this.groupService.getGroupsByCourseId(courseId));
+    }
 }
--- src/main/java/com/example/codeE/service/group/GroupService.java
+++ src/main/java/com/example/codeE/service/group/GroupService.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import com.example.codeE.model.group.Group;
 import com.example.codeE.model.user.User;
+import com.example.codeE.request.group.CreateGroupRequest;
 import com.example.codeE.service.common.CommonService;
 
-public interface GroupService extends CommonService<Group> {
+public interface GroupService extends CommonService<Group, CreateGroupRequest> {
     Boolean updateGroupById(String groupId, Group updatedGroup);
     List<Group> getGroupsByCourseId(String courseId);
     List<User> getUsersInGroup(String groupId);
