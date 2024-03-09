cba94802033a26a7fc4c55edf09580f23795d799
TungPhan2808 <phanmanhtung123@gmail.com>
2024-03-09 16:47:46 +0700

    fix[WW-88] return unsuccessful imported courses after import-courses.

--- src/main/java/com/example/codeE/service/course/CourseService.java
+++ src/main/java/com/example/codeE/service/course/CourseService.java
@@ -5,10 +5,13 @@ import com.example.codeE.request.course.CourseResponse;
 import com.example.codeE.request.course.CreateCourseRequest;
 import com.example.codeE.request.course.UpdateCourseRequest;
 import com.example.codeE.service.common.CommonService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 public interface CourseService extends CommonService<CourseResponse, CreateCourseRequest> {
     Course updateById(String id, UpdateCourseRequest update);
-    boolean importByExcel(MultipartFile file);
+    ResponseEntity<Map<String, String>> importByExcel(MultipartFile file);
     Boolean checkCourseExistById(String groupId);
 }
