977bdc4dd1b1b92b1e19c615374a9e5b32d94504
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-03-09 18:35:43 +0700

    fix[WW-85] update CORS

--- src/main/java/com/example/codeE/security/WebConfig.java
+++ src/main/java/com/example/codeE/security/WebConfig.java
@@ -1,16 +1,29 @@
 package com.example.codeE.security;
 
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4000", "http://localhost:4001")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4000", "http://localhost:4001")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
     }
+
 }
+
+// .allowedOrigins(cors.getAllowedOrigins())
+//         .allowedMethods(cors.getAllowedMethods())
+//         .maxAge(cors.getMaxAge())
+//         .allowedHeaders(cors.getAllowedHeaders())
+//         .exposedHeaders(cors.getExposedHeaders());
