33ded1ed9ae78734b492bd7dc408e2f065af3f58
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-03-11 22:51:30 +0700

    [WW-155] handle error when username/password wrong

--- src/main/java/com/example/codeE/controller/AuthController.java
+++ src/main/java/com/example/codeE/controller/AuthController.java
@@ -26,21 +26,21 @@ public class AuthController {
     @RequestMapping(value = "/login/user", method = RequestMethod.POST)
     public ResponseEntity<?> signInUser(@RequestBody LoginRequest signInRequest, HttpServletResponse response) {
         var result = authenService.signIn(signInRequest, response);
-        if (result.getStatusCode() == 200) {
-            return ResponseEntity.status(HttpStatus.OK).body(result);
-        } else
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+        if (result.getStatusCode() == 200) return ResponseEntity.status(HttpStatus.OK).body(result);
+        else if(result.getStatusCode() == 403) return ResponseEntity.status(HttpStatus.FORBIDDEN).body(result);
+        else if(result.getStatusCode() == 404) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+        else if (result.getStatusCode() == 401) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+        else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
     }
     @PostMapping
     @RequestMapping(value = "/login/admin", method = RequestMethod.POST)
     public ResponseEntity<?> signInAdmin(@RequestBody LoginRequest signInRequest, HttpServletResponse response) {
         var result = authenService.signInAdmin(signInRequest, response);
-        if (result.getStatusCode() == 200) {
-            return ResponseEntity.status(HttpStatus.OK).body(result);
-        } else if(result.getStatusCode() == 403)
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(result);
-        else
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
+        if (result.getStatusCode() == 200) return ResponseEntity.status(HttpStatus.OK).body(result);
+        else if(result.getStatusCode() == 403) return ResponseEntity.status(HttpStatus.FORBIDDEN).body(result);
+        else if(result.getStatusCode() == 404) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+        else if (result.getStatusCode() == 401) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+        else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
     }
     @PostMapping
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
