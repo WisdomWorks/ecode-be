235aa3ba26f3622d29af0a6d09502065120daa08
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-03-12 20:37:14 +0700

    feat[WW-87] Add Send Email Helper

---
+++ src/main/java/com/example/codeE/model/common/GmailCredential.java
@@ -0,0 +1,10 @@
+package com.example.codeE.model.common;
+import jakarta.annotation.Nullable;
+
+public record GmailCredential(@Nullable String client_id,
+                              @Nullable String client_secret,
+                              @Nullable String refresh_token,
+                              @Nullable String grant_type,
+                              @Nullable String access_token,
+                              @Nullable String userEmail) {
+}
