325e4eea0fc6f4e14b3a87b82360831c8ebc61ae
TungPhan2808 <phanmanhtung123@gmail.com>
2024-03-16 00:55:50 +0700

    fix[WW-79] change update response

--- src/main/java/com/example/codeE/controller/CourseController.java
+++ src/main/java/com/example/codeE/controller/CourseController.java
@@ -70,14 +70,14 @@ public class CourseController {
     @RequestMapping(value = "", method = RequestMethod.PUT)
     public ResponseEntity<?> updateById(@Valid @RequestBody UpdateCourseRequest updates){
         Course course = courseService.updateById(updates.getCourseId(), updates);
-        return ResponseEntity.status(HttpStatus.CREATED).body(course);
+        return ResponseEntity.ok(Map.of("message" , "Update course successfully"));
     }
 
     @PutMapping
     @RequestMapping(value = "students", method = RequestMethod.PUT)
     public ResponseEntity<?> updateStudentsInCourse(@Valid @RequestBody UpdateStudentsToCourseRequest request) {
         var result = courseStudentService.updateStudentsInCourse(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @DeleteMapping
