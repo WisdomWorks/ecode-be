1bdd1d32909a431467b0d27686876ec75547b212
Noro911 <hoainhan.lhp@gmail.com>
2024-03-22 08:52:36 +0700

    fix: change port server

--- src/main/java/com/example/codeE/controller/JudgeController.java
+++ src/main/java/com/example/codeE/controller/JudgeController.java
@@ -22,7 +22,7 @@ public class JudgeController {
 
     private static final String HOST = "127.0.0.1";
 
-    private static final int PORT = 8080;
+    private static final int PORT = 9998;
 
     @Autowired
     private ClientService clientService;
