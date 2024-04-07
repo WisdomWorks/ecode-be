a3b2d9a4bbade08b605d545b53fe99b48b003536
TungPhan2808 <phanmanhtung123@gmail.com>
2024-04-07 14:20:12 +0700

    feat[dev-stef]: more api of file exercise and submission

--- src/main/java/com/example/codeE/model/exercise/FileExercise.java
+++ src/main/java/com/example/codeE/model/exercise/FileExercise.java
@@ -1,6 +1,7 @@
 package com.example.codeE.model.exercise;
 
 import com.example.codeE.request.exercise.file.CreateFileExerciseRequest;
+import com.example.codeE.request.exercise.file.UpdateFileExerciseRequest;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,8 +24,15 @@ public class FileExercise extends Exercise{
     private String question;
 
     public FileExercise(CreateFileExerciseRequest request){
-        super(request.getTopicId(), request.getExerciseName(), request.getKey(), request.getStartTime(), request.getEndTime(), request.getDurationTime(), request.getReAttempt(), "file",request.getExerciseDescription(), false,
-                new ArrayList<String>());
+        super(request.getTopicId(), request.getExerciseName(), request.getKey(), request.getStartTime(), request.getEndTime(), request.getDurationTime(), request.getReAttempt(), "file",
+                request.getExerciseDescription(), false, new ArrayList<String>());
+        this.question = request.getQuestion();
+    }
+
+    public FileExercise(String exerciseId, UpdateFileExerciseRequest request, boolean isShowAll, List<String> groups){
+        super(exerciseId,
+                request.getTopicId(), request.getExerciseName(), request.getKey(), request.getStartTime(), request.getEndTime(), request.getDurationTime(), request.getReAttempt(), "file",
+                request.getExerciseDescription(), isShowAll, groups);
         this.question = request.getQuestion();
     }
 }
--- src/main/java/com/example/codeE/service/exercise/submission/FileSubmissionService.java
+++ src/main/java/com/example/codeE/service/exercise/submission/FileSubmissionService.java
@@ -2,8 +2,14 @@ package com.example.codeE.service.exercise.submission;
 
 import com.example.codeE.model.exercise.FileSubmission;
 import com.example.codeE.request.exercise.file.CreateFileSubmissionRequest;
+import com.example.codeE.request.exercise.file.response.FileSubmissionsResponse;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface FileSubmissionService {
     FileSubmission createSubmission(CreateFileSubmissionRequest request, MultipartFile file);
+    List<FileSubmissionsResponse> getFileSubmissionsByExerciseId(String exerciseId);
+    FileSubmissionsResponse getFileSubmissionById(String submissionId);
+    List<FileSubmission> getFileSubmissionByUserId(String exerciseId, String userId);
 }
