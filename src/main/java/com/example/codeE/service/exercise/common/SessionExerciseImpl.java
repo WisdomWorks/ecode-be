2b67569c63b262d4d29cf1dcfb900ffec65a2952
Ho Xuan Hieu <hxhieu123@gmail.com>
2024-04-16 02:36:04 +0700

    fix: update session exercise

--- src/main/java/com/example/codeE/service/exercise/common/SessionExerciseImpl.java
+++ src/main/java/com/example/codeE/service/exercise/common/SessionExerciseImpl.java
@@ -16,21 +16,27 @@ public class SessionExerciseImpl implements SessionExerciseService{
     private SessionExerciseRepository sessionExerciseRepository;
 
     @Override
-    public void removeSession(HttpServletResponse response, HttpServletRequest request, String exerciseId) {
+    public void removeSession(HttpServletResponse response, HttpServletRequest request) {
         var sessionList = this.sessionExerciseRepository.findAll();
         String loginId = "";
         Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
+        try {
+            if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if ("LoginSessionId".equals(cookie.getName())) {
                     loginId = cookie.getValue();
                 }
             }
-        }
-        for (var item: sessionList){
-            if(Objects.equals(item.getLoginId(), loginId)){
-                this.sessionExerciseRepository.delete(item);
+            for (var item : sessionList) {
+                if (Objects.equals(item.getLoginId(), loginId)) {
+                    this.sessionExerciseRepository.deleteById(item.getSessionId());
+                }
             }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+
     }
 }
